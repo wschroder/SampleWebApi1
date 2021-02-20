a4a791da73ee198b40a0ffb13ea50ec0ae102517
Wayne Schroder <wschroder@outlook.com>
2021-02-19 20:39:02 -0500

    Fix deployment script

--- scripts/deploy-app.ps1
+++ scripts/deploy-app.ps1
@@ -1,5 +1,4 @@
 $gitRepo = "https://github.com/wschroder/SampleWebApi1.git"
-$webAppName = "SampleWebApi$(Get-Random)"
 $location = "East US"
 $env = "dev"
 $rgName = "$($env)-gfc-permitter"
@@ -7,20 +6,26 @@ $servicePlan = "$($env)-gfc-fire-permitter-plan"
 $apiName = "$($env)-gfc-fire-permitter-api"
 $webName = "$($env)-gfc-fire-permitter-web"
 
+Clear-Host
+
 # (env)-(company)-(domain)-(item)-(type)
 
-New-AzResourceGroup -Name $rgName -Location $location
+# New-AzResourceGroup -Name $rgName -Location $location
 
-New-AzAppServicePlan -Name $servicePlan -Location $location -ResourceGroupName $rgName -Tier Free
+# New-AzAppServicePlan -Name $servicePlan -Location $location -ResourceGroupName $rgName -Tier Free
 
-New-AzWebApp -Name $webName -Location $location -AppServicePlan $servicePlan  -ResourceGroupName $rgName
+# New-AzWebApp -Name $webName -Location $location -AppServicePlan $servicePlan  -ResourceGroupName $rgName
 
-New-AzWebApp -Name $apiName -Location $location -AppServicePlan $servicePlan  -ResourceGroupName $rgName
+# New-AzWebApp -Name $apiName -Location $location -AppServicePlan $servicePlan  -ResourceGroupName $rgName
 
 $propertiesObject = @{
     repoUrl = $gitRepo;
     branch = "master";
-    isManualIntegration = true
+    isManualIntegration = "true"
 }
 
-Set-AzResource -Properties $propertiesObject -ResourceGroupName $rgName -ResourceType Microsoft.Web/sites/sourcecontrols -ResourceName $webAppName/web -ApiVersion 2015-08-01 -Force
+Write-Host "Deploying web api..."
+
+# Set-AzResource -Properties $propertiesObject -ResourceGroupName $rgName -ResourceType Microsoft.Web/sites/sourcecontrols  -ResourceName $apiName  -ApiVersion 2015-08-01 -Force
+Set-AzResource -Properties $propertiesObject -ResourceGroupName $rgName -ResourceType Microsoft.Web/sites  -ResourceName $apiName  -ApiVersion 2015-08-01 -Force
+
